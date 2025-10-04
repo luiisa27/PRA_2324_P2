1d094e8a9d1053098aca80e303ce25558cdf95e6
Luisa Arango <lfarator@etsinf.upv.es>
2025-10-04 20:34:00 +0200

    BusquedaBinaria_INV añadida y main modificado

--- DyV.h
+++ DyV.h
@@ -12,7 +12,7 @@ return -1;
 
 int medio = (ini + fin)/2;
 
-if(medio == x){
+if(vect[medio] == x){
 return medio;
 }
 
@@ -25,3 +25,24 @@ return BusquedaBinaria(x,vect,medio+1,fin);
 }
 
 }
+
+template<typename T>
+int BusquedaBinaria_INV(int x, std::vector<T> vect, int ini, int fin) {
+    if (ini > fin) {
+        cout << "No se encuentra en el array" << endl;
+        return -1;
+    }
+
+    int medio = (ini + fin) / 2;
+
+    if (vect[medio] == x) {
+        return medio;
+    }
+    else if (vect[medio] < x) {
+        return BusquedaBinaria_INV(x, vect, ini, medio - 1);
+    }
+    else {
+        return BusquedaBinaria_INV(x, vect, medio + 1, fin);
+    }
+}
+
--- test_DyV.cpp
+++ test_DyV.cpp
@@ -5,5 +5,10 @@ using namespace std;
 
 int main(){
 vector<int> vect = { 1, 2, 4, 9, 11, 15, 21, 25, 30};
-return BusquedaBinaria(4,vect,0,8);
+    cout << "Busqueda ascendente (4): " << BusquedaBinaria(4, vect, 0, vect.size() - 1) << endl;
+
+    vector<int> vectDesc = {30, 25, 21, 15, 11, 9, 4, 2, 1};
+    cout << "Busqueda descendente (4): " << BusquedaBinaria_INV(4, vectDesc, 0, vectDesc.size() - 1) << endl;
+
+return 0;
 }
